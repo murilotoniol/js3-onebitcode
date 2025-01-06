19e1868d8cdbb95c6e606a5de6b0b16e3d629b30
Murilo Besson <murilo.besson@outlook.com>
2025-01-06 16:25:38 -0300

    Calculadora 1

--- calculadora/index.js
+++ calculadora/index.js
@@ -5,6 +5,19 @@ const inputResultado = document.getElementById('resultado')
 
 const teclasPermitidas = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
+    charKeyBtn.addEventListener('click', function (){
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function(){
+    input.value=''
+    input.focus
+})
+
 input.addEventListener('keydown', function(ev){
     ev.preventDefault()
     if (teclasPermitidas.includes(ev.key)){
@@ -14,4 +27,14 @@ input.addEventListener('keydown', function(ev){
     if (ev.key === 'Backspace'){
         input.value = input.value.slice(0, -1)
     }
+    if (ev.key === 'Enter'){
+        calcular()
+    }
 })
+
+document.getElementById('equal').addEventListener('click', calcular)
+
+function calcular(){
+    const resultado = eval(input.value)
+    inputResultado.value = resultado
+}
