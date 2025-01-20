884e234f66f39111b0e437bcd270f3f006b799d3
Murilo Besson <murilo.besson@outlook.com>
2025-01-20 17:10:19 -0300

    Revisitando aulas

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-function criarParagrafo() {
-  let para = document.createElement("p");
-  para.textContent = "Você clicou no botão!";
-  document.body.appendChild(para);
+function addContact(){
+  const contactSection = document.getElementById('contacts-list') 
+  const h3 = document.createElement('h3')
+  h3.innerText = "Contato"
+
+  const ul = document.createElement('ul')
+
+  const nameLi = document.createElement('li')
+  nameLi.innerText = "Nome: " 
+  const nameInput = document.createElement('input')
+  nameInput.type = 'text'
+  nameInput.name = 'fullname'
+  nameLi.appendChild(nameInput)
+  ul.appendChild(nameLi)
+  ul.appendChild(document.createElement('br'))
+
+  const phoneLi = document.createElement('li')
+  phoneLi.innerText = "Phone: " 
+  const phoneInput = document.createElement('input')
+  phoneInput.type = 'text'
+  phoneInput.name = 'phone'
+  phoneLi.appendChild(phoneInput)
+  ul.appendChild(phoneLi)
+  ul.appendChild(document.createElement('br'))
+
+  addressLi = document.createElement('li')
+  addressLi.innerHTML = '<label for="addres">Endereço: </label>'
+  const addressInput = document.createElement('input')
+  addressInput.type = 'text'
+  addressInput.name = 'address'
+  addressLi.appendChild(addressInput)
+  ul.appendChild(addressLi)
+  ul.appendChild(document.createElement('br'))
+
+  contactSection.append(h3, ul)
 }
 
-const botoes = document.getElementById("botao");
+function removeContact(){
+  const contactSection = document.getElementById('contacts-list')
 
-botoes.addEventListener("click", criarParagrafo)
+  const titles = document.getElementsByTagName('h3')
+  const contacts = document.getElementsByTagName('ul')
+
+  contactSection.removeChild(titles[titles.length - 1])
+  contactSection.removeChild(contacts[contacts.length - 1])
+}
