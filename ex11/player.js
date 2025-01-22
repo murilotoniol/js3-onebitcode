778945be181c5fc0d9d8039149922786418ebf87
Murilo Besson <murilo.besson@outlook.com>
2025-01-22 09:39:13 -0300

    Refazendo ex11

--- ex11/player.js
+++ ex11/player.js
@@ -1,30 +1,31 @@
 function addPlayer(){
     const position = document.getElementById('position').value
     const name = document.getElementById('name').value
-    const number= document.getElementById('number').value
+    const number = document.getElementById('number').value
 
-    const confirmation = confirm("Deseja escalar o jogador "+name+" na posição "+position+"?")
-    if(confirmation){
+    const confirmation = confirm(`Escalar ${name} como ${position}?`)
+
+    if (confirmation){
         const teamList = document.getElementById('team-list')
-        const playerLi = document.createElement('li')
-        playerLi.id = "player-"+number
-        playerLi.innerText = position+": "+name+" ("+number+")"
-        teamList.appendChild(playerLi)
+        const playerItem = document.createElement('li')
+        playerItem.id = 'player-' + number
+        playerItem.innerText = `${position}: ${name} (${number})`
+        teamList.appendChild(playerItem)
 
-        document.getElementById('position').value=""
-        document.getElementById('name').value=""
-        document.getElementById('number').value=""
+        document.getElementById('position').value = ""
+        document.getElementById('name').value = ""
+        document.getElementById('number').value = ""
     }
 }
 
 function removePlayer(){
-
     const number = document.getElementById('numberToRemove').value
     const playerToRemove = document.getElementById('player-'+number)
-    const confirmation = confirm("Deseja remover o jogador "+playerToRemove.innerText+"?")
+
+    const confirmation = confirm(`Remover o jogador ${playerToRemove.innerText}?`)
 
     if(confirmation){
-        document.getElementById("team-list").removeChild(playerToRemove)
-        document.getElementById("numberToRemove").value = ""
+        document.getElementById('team-list').removeChild(playerToRemove)
+        document.getElementById('numberToRemove').value = ""
     }
 }
